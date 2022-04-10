f182f73cdac5ef816db3fa7c0ab6bdb5526157be
ken1flan <ken1flan@gmail.com>
2022-04-10 09:05:59 +0900

    メモの追加

---
+++ lib/models/memo.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+class Memo {
+  late String name;
+  late File labelImage;
+  late File specImage;
+  late File otherImage;
+  late String shopName;
+  late DateTime tappedOn;
+  late List<String> keywords;
+  late int score;
+  late String body;
+}
