df7d9ca399ac2d5b12a862c20662ffcf42c70bb5
ken1flan <ken1flan@gmail.com>
2022-04-11 08:37:43 +0900

    デバッグ用データを設定する関数を設置

--- lib/widgets/memo_list_page.dart
+++ lib/widgets/memo_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uchi_sake/models/memo.dart';
 
 class MemoListPage extends StatefulWidget {
   const MemoListPage({Key? key}) : super(key: key);
@@ -8,21 +9,34 @@ class MemoListPage extends StatefulWidget {
 }
 
 class _MemoListPageState extends State<MemoListPage> {
+  List<Memo> memos = [];
+
+  void _setTestMemos() {
+    for (int i = 0; i < 10; i++) {
+      Memo memo = Memo();
+      memo.name = "お酒${i.toString()}";
+      memo.tappedOn = DateTime.now();
+      memo.score = i % 5 + 1;
+      memos.add(memo);
+    }
+  }
+
   void _createMemo() {
     // TODO: 新規メモ作成ページを表示
   }
 
   @override
   Widget build(BuildContext context) {
+    _setTestMemos();
     return Scaffold(
       appBar: AppBar(
-        title: Text('飲んだお酒のメモ'),
+        title: const Text('飲んだお酒のメモ'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
+          children: const <Widget>[
+            Text(
               'To Do',
             ),
           ],
