fc9ed6e1960d8f1a9c983ae894ce960ce4c41ca3
ken1flan <ken1flan@gmail.com>
2022-04-21 09:10:08 +0900

    開栓日をカレンダーで選択

--- lib/widgets/memo_edit_page.dart
+++ lib/widgets/memo_edit_page.dart
@@ -16,6 +16,7 @@ class _MemoEditPageState extends State<MemoEditPage> {
     memo = Memo();
     memo.name = 'おさけのなまえ';
     memo.purchaceStore = 'なんとか酒店';
+    memo.tappedOn = DateTime.now();
     memo.keywordsString = '日本酒 純米 原酒 辛口';
     memo.body = 'たいへん美味しかった！';
 
@@ -25,7 +26,22 @@ class _MemoEditPageState extends State<MemoEditPage> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16),
-          child: Column(children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            InkWell(
+              child: Text(
+                  "開栓日 ${memo.tappedOn.year}/${memo.tappedOn.month}/${memo.tappedOn.day}"),
+              onTap: () async {
+                DateTime? picked = await _selectDate(memo, context);
+                if (picked != null) {
+                  setState(() {
+                    memo.tappedOn = picked;
+                    // TODO: 保存
+                  });
+                }
+              },
+            ),
+            const SizedBox(height: 15),
             TextFormField(
               initialValue: memo.name,
               key: const ValueKey('memoNameTextField'),
@@ -69,5 +85,17 @@ class _MemoEditPageState extends State<MemoEditPage> {
             ),
           ]),
         ));
+  }
+
+  Future<DateTime?> _selectDate(Memo memo, BuildContext context) async {
+    DateTime initialDate = memo.tappedOn;
+    DateTime firstDate = DateTime.now().add(const Duration(days: -360));
+    DateTime lastDate = DateTime.now().add(const Duration(days: 360));
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: initialDate,
+        firstDate: firstDate,
+        lastDate: lastDate);
+    return picked;
   }
 }
