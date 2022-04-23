ef50ef4b6c439e72ee479bfb396acc2ac2ca81f4
ken1flan <ken1flan@gmail.com>
2022-04-24 08:55:16 +0900

    ウィジェットを直接テストするように変更
    毎回mainから対象のページを起動するのは大変すぎるので、
    単体でテストするように変更します。

--- test/widgets/memo_list_page_test.dart
+++ test/widgets/memo_list_page_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:uchi_sake/main.dart';
+import 'package:flutter/material.dart';
+import 'package:uchi_sake/widgets/memo_list_page.dart';
 
 void main() {
   testWidgets('メモ一覧ページのテスト', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    const memoListPage = MemoListPage();
+    await tester.pumpWidget(const MaterialApp(home: memoListPage));
 
     expect(find.text('飲んだお酒のメモ'), findsOneWidget);
   });
