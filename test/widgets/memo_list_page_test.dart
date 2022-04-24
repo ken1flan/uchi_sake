6ae12f1e121741eb3006cd6c1209f36852234a50
KUDO Ken-ichi <ken1flan@gmail.com>
2022-04-24 09:08:24 +0900

    テストを改善 (#9)
    * ウィジェットを直接テストするように変更
    
    毎回mainから対象のページを起動するのは大変すぎるので、
    単体でテストするように変更します。
    
    * 共通の初期化・終了処理を設定できるようにします。
    
    あとで入れるデータベースのセットアップなどで使います。

--- test/widgets/memo_list_page_test.dart
+++ test/widgets/memo_list_page_test.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:uchi_sake/main.dart';
+import 'package:flutter/material.dart';
+import '../test_helper.dart';
+import 'package:uchi_sake/widgets/memo_list_page.dart';
 
 void main() {
-  testWidgets('メモ一覧ページのテスト', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+  wrapTest(() {
+    testWidgets('メモ一覧ページのテスト', (WidgetTester tester) async {
+      const memoListPage = MemoListPage();
+      await tester.pumpWidget(const MaterialApp(home: memoListPage));
 
-    expect(find.text('飲んだお酒のメモ'), findsOneWidget);
+      expect(find.text('飲んだお酒のメモ'), findsOneWidget);
+    });
   });
 }
