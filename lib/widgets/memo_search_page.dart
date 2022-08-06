5b53688fd1addbb42c22eaa7e0fa2c3da7e79418
ken1flan <ken1flan@gmail.com>
2022-08-07 08:44:17 +0900

    AppBarにテキストボックスを配置

--- lib/widgets/memo_search_page.dart
+++ lib/widgets/memo_search_page.dart
@@ -11,9 +11,19 @@ class _MemoSearchPageState extends State<MemoSearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('検索')),
+        appBar: AppBar(title: _searchTextField()),
         body: Center(
           child: Column(children: const [Text('みつかりません')]),
         ));
+  }
+
+  Widget _searchTextField() {
+    return const TextField(
+      autofocus: true,
+      textInputAction: TextInputAction.search,
+      decoration: InputDecoration(
+        hintText: '検索',
+      ),
+    );
   }
 }
