3985295ea9b7faf061b3bbef2fa7b996596b3f20
KUDO Ken-ichi <ken1flan@gmail.com>
2022-08-08 00:05:39 +0900

    メモ一覧にあるメモカードをクラスとして分離 (#39)
    * Dartのバージョンを2.17以上に設定
    
    コンストラクタでsuper.keyを使いたかったため。
    
    * MemoCardを追加

--- lib/widgets/memo_list_page.dart
+++ lib/widgets/memo_list_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
 import 'package:uchi_sake/common.dart';
-import 'package:uchi_sake/helpers.dart';
 import 'package:uchi_sake/models/memo.dart';
 import 'package:uchi_sake/widgets/memo_edit_page.dart';
+import 'package:uchi_sake/widgets/memo_card.dart';
 
 class MemoListPage extends StatefulWidget {
   const MemoListPage({Key? key}) : super(key: key);
@@ -69,53 +69,9 @@ class _MemoListPageState extends State<MemoListPage> {
             )
           : ListView(
               children: memos.map((Memo memo) {
-              // return Card(child: Text(memo.name));
-              return Card(
-                child: InkWell(
+              return MemoCard(memo,
                   onTap: () => _openMemo(memo, context),
-                  onLongPress: () => _deleteMemo(memo, context),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: memo.labelImage == null
-                            ? const Image(
-                                image: AssetImage('assets/images/1px.png'),
-                                width: 64,
-                                height: 64,
-                                fit: BoxFit.cover,
-                              )
-                            : Image.file(
-                                memo.labelImage!,
-                                width: 64,
-                                height: 64,
-                                fit: BoxFit.cover,
-                              ),
-                      ),
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              dateTime2yyyymmdd(memo.tappedOn),
-                              style: const TextStyle(
-                                  color: Colors.grey, fontSize: 15),
-                            ),
-                            Text(
-                              memo.name ?? '',
-                              style: const TextStyle(fontSize: 20),
-                              overflow: TextOverflow.ellipsis,
-                              softWrap: true,
-                            ),
-                            stars(memo.score),
-                          ],
-                        ),
-                      ),
-                    ]),
-                  ),
-                ),
-              );
+                  onLongPress: () => _deleteMemo(memo, context));
             }).toList()),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _openMemo(Memo(), context),
