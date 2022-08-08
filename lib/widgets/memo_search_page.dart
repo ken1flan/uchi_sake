aaff6c5b4834e9d4a3ee799f279b4ac0d5106323
ken1flan <ken1flan@gmail.com>
2022-08-08 23:39:28 +0900

    メモを全件表示
    編集と削除もできるようにした。

--- lib/widgets/memo_search_page.dart
+++ lib/widgets/memo_search_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:isar/isar.dart';
+import 'package:uchi_sake/common.dart';
+import 'package:uchi_sake/models/memo.dart';
+import 'package:uchi_sake/widgets/memo_card.dart';
+import 'package:uchi_sake/widgets/memo_edit_page.dart';
 
 class MemoSearchPage extends StatefulWidget {
   const MemoSearchPage({Key? key}) : super(key: key);
@@ -10,11 +15,27 @@ class MemoSearchPage extends StatefulWidget {
 class _MemoSearchPageState extends State<MemoSearchPage> {
   @override
   Widget build(BuildContext context) {
+    List<Memo> memos =
+        isar.memos.where(sort: Sort.desc).anyTappedOn().findAllSync();
+
     return Scaffold(
-        appBar: AppBar(title: _searchTextField()),
-        body: Center(
-          child: Column(children: const [Text('みつかりません')]),
-        ));
+      appBar: AppBar(title: _searchTextField()),
+      body: memos.isEmpty
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text('まだありません。'),
+                ],
+              ),
+            )
+          : ListView(
+              children: memos.map((Memo memo) {
+              return MemoCard(memo,
+                  onTap: () => _openMemo(memo, context),
+                  onLongPress: () => _deleteMemo(memo, context));
+            }).toList()),
+    );
   }
 
   Widget _searchTextField() {
@@ -25,5 +46,40 @@ class _MemoSearchPageState extends State<MemoSearchPage> {
         hintText: '検索',
       ),
     );
+  }
+
+  void _openMemo(Memo memo, BuildContext context) async {
+    await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MemoEditPage(memo),
+        ));
+    setState(() {});
+  }
+
+  void _deleteMemo(Memo memo, BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            content: Text('「${memo.name}」を削除しますか？'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: const Text('いいえ'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  setState(() {
+                    memo.destroy();
+                  });
+                  Navigator.of(context).pop();
+                },
+                child: const Text('はい'),
+              )
+            ],
+          );
+        });
   }
 }
